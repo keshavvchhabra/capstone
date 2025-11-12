bc99576686898a627ecf3e7916e2e36f0d3e5a44
KeshavNST <keshav.@Keshavs-MacBook-Pro-2.local>
2025-11-12 19:15:07 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://capstone-git-main-keshavvchhabras-projects.vercel.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
